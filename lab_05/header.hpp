1e748a59d22000ab6e2af9c64569d99e852742ab
Vive_BTL <vovat@LAPTOP-GV1VTS1D>
2025-11-14 04:33:33 +0300

    1

--- lab_05/header.hpp
+++ lab_05/header.hpp
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 
+#include <iostream>
 #include <string>
 #include <vector>
 
--- lab_05/task1.cpp
+++ lab_05/task1.cpp
@@ -1,6 +1,4 @@
 
-#include <vector>
-#include <string>
 #include "header.hpp"
 
 
